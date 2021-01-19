713b431e6da9aed0a163cb5b48fd2d96f602242f
asus <asus@DESKTOP-HL11OFI>
2021-01-20 11:46:45 +1300

    model

---
+++ dbo.Model.data.sql
@@ -0,0 +1,11 @@
+﻿SET IDENTITY_INSERT [dbo].[Model] ON
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (1, N'GSX-R1000RA', N'2010', 4, NULL, 5)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (2, N'KATANA SUZUKIFEST', N'2012', 4, NULL, 5)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (3, N'DR-Z400E', N'2013', 4, NULL, 5)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (4, N'CHEROKEE', N'2020', 1, NULL, 6)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (5, N'Compass', N'2019', 1, NULL, 6)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (6, N'MX-5', N'2018', 3, NULL, 8)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (7, N'Mazda-6', N'2019', 3, NULL, 8)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (8, N'Camry Hybrid GX', N'2019', 2, NULL, 7)
+INSERT INTO [dbo].[Model] ([Id], [Model_Name], [Model_Year], [Company_Id], [CompanyId], [DealerId]) VALUES (9, N'gr supra', N'2020', 2, NULL, 7)
+SET IDENTITY_INSERT [dbo].[Model] OFF
